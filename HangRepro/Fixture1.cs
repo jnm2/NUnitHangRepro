50f6a8309851a3af783ef8d0f3c3e49d20219ead
jnm2 <me@jnm2.com>
2019-07-01 19:51:48 -0400

    Fix test names to show actual order (verified 16%)

--- HangRepro/Fixture1.cs
+++ HangRepro/Fixture1.cs
@@ -7,56 +7,45 @@ namespace HangRepro
     public static class Fixture1
     {
         [Test]
-        public static void Test_1()
-        {
-            Thread.Sleep(385);
-        }
+        public static void Test_01() => Thread.Sleep(385);
 
         [Test]
-        public static void Test_2()
-        {
-            Thread.Sleep(45);
-        }
+        public static void Test_02() => Thread.Sleep(45);
 
         [Test]
-        public static void Tests_3_to_14([Range(1, 12)] int x)
-        {
-            Thread.Sleep(20);
-        }
+        public static void Test_03() => Thread.Sleep(66);
 
         [Test]
-        public static void Tests_15_to_26([Range(1, 12)] int x)
-        {
-        }
+        public static void Test_04() => Thread.Sleep(80);
 
         [Test]
-        public static void Test_27() => Thread.Sleep(66);
+        public static void Test_05() => Thread.Sleep(75);
 
         [Test]
-        public static void Test_28() => Thread.Sleep(80);
+        public static void Test_06() => Thread.Sleep(82);
 
         [Test]
-        public static void Test_29() => Thread.Sleep(75);
+        public static void Test_07() => Thread.Sleep(69);
 
         [Test]
-        public static void Test_30() => Thread.Sleep(82);
+        public static void Test_08() => Thread.Sleep(75);
 
         [Test]
-        public static void Test_31() => Thread.Sleep(69);
-
-        [Test]
-        public static void Test_32() => Thread.Sleep(75);
-
-        [Test]
-        public static void Test_33()
+        public static void Test_09()
         {
             new StackTrace(true);
         }
 
         [Test]
-        public static void Test_34()
+        public static void Test_10()
         {
             new StackTrace(true);
+        }
+
+        [Test]
+        public static void Tests_at_end([Range(1, 24)] int x)
+        {
+            if (x > 12) Thread.Sleep(20);
         }
     }
 }
