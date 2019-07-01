40280c42f33e92aa47dd78345bf58f4a87bd1ac6
jnm2 <me@jnm2.com>
2019-07-01 18:42:41 -0400

    Consolidated remaining fixture classes (verified 17/100)

--- HangRepro/Fixture1.cs
+++ HangRepro/Fixture1.cs
@@ -1,3 +1,4 @@
+using System.Diagnostics;
 using System.Threading;
 using NUnit.Framework;
 
@@ -26,6 +27,53 @@ namespace HangRepro
         [Test]
         public static void Tests_15_to_26([Range(1, 12)] int x)
         {
+        }
+
+        [Test]
+        public static void Test_27() => Thread.Sleep(66);
+
+        [Test]
+        public static void Test_28() => Thread.Sleep(80);
+
+        [Test]
+        public static void Test_29() => Thread.Sleep(75);
+
+        [Test]
+        public static void Test_30() => Thread.Sleep(82);
+
+        [Test]
+        public static void Test_31() => Thread.Sleep(69);
+
+        [Test]
+        public static void Test_32() => Thread.Sleep(75);
+
+        [Test]
+        public static void Test_33()
+        {
+            new StackTrace(true);
+        }
+
+        [Test]
+        public static void Test_34()
+        {
+            new StackTrace(true);
+        }
+
+        [Test]
+        public static void Test_35()
+        {
+            new StackTrace(true);
+        }
+
+        [Test]
+        public static void Test_36()
+        {
+        }
+
+        [Test]
+        public static void Test_37()
+        {
+            new StackTrace(true);
         }
     }
 }
