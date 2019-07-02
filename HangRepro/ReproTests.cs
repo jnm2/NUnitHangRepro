91f62c264e5485f5994100784c3a01563792d324
jnm2 <me@jnm2.com>
2019-07-01 21:13:47 -0400

    Simplified tests by noticing that first two tests usually have the same worker as racing tests (12%)

--- HangRepro/ReproTests.cs
+++ HangRepro/ReproTests.cs
@@ -10,33 +10,16 @@ namespace HangRepro
     public static class ReproTests
     {
         [Test]
-        public static void A__Timing_test_1() => Thread.Sleep(45);
-
-        [Test]
-        public static void A__Timing_test_2() => Thread.Sleep(65);
-
-        [Test]
-        public static void A__Timing_test_3() => Thread.Sleep(80);
-
-        [Test]
-        public static void A__Timing_test_4() => Thread.Sleep(70);
-
-        [Test]
-        public static void A__Timing_test_5() => Thread.Sleep(80);
-
-        [Test]
-        public static void A__Timing_test_6() => Thread.Sleep(70);
-
-        [Test]
-        public static void B__Racing_tests([Range(1, 2)] int _)
+        public static void A([Values(45, 65)] int delay)
         {
+            Thread.Sleep(delay);
             new StackTrace(true);
         }
 
         [Test]
-        public static void C__Ending_tests([Range(1, 20)] int _)
+        public static void B([Range(1, 20)] int _)
         {
-            Thread.Sleep(10);
+            Thread.Sleep(20);
         }
     }
 }
