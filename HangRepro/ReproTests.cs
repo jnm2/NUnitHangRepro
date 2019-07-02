0bd472a9b70ca39caa927464d8ecb5717ae8e72e
jnm2 <me@jnm2.com>
2019-07-01 21:03:25 -0400

    Round timings (12%)

--- HangRepro/ReproTests.cs
+++ HangRepro/ReproTests.cs
@@ -13,7 +13,7 @@ namespace HangRepro
         public static void A__Timing_test_1() => Thread.Sleep(45);
 
         [Test]
-        public static void A__Timing_test_2() => Thread.Sleep(66);
+        public static void A__Timing_test_2() => Thread.Sleep(65);
 
         [Test]
         public static void A__Timing_test_3() => Thread.Sleep(80);
@@ -22,10 +22,10 @@ namespace HangRepro
         public static void A__Timing_test_4() => Thread.Sleep(75);
 
         [Test]
-        public static void A__Timing_test_5() => Thread.Sleep(82);
+        public static void A__Timing_test_5() => Thread.Sleep(80);
 
         [Test]
-        public static void A__Timing_test_6() => Thread.Sleep(69);
+        public static void A__Timing_test_6() => Thread.Sleep(70);
 
         [Test]
         public static void B__Racing_tests([Range(1, 2)] int _)
