aaa49f959854462837fe3d45c2eb6cf85562b8ea
jnm2 <me@jnm2.com>
2019-07-01 20:07:20 -0400

    Reduce level of parallelism and rename tests again (16%)

--- HangRepro/ReproTests.cs
+++ HangRepro/ReproTests.cs
@@ -3,50 +3,41 @@ using System.Threading;
 using NUnit.Framework;
 
 [assembly: Parallelizable(ParallelScope.Children)]
-[assembly: LevelOfParallelism(8)]
+[assembly: LevelOfParallelism(7)]
 
 namespace HangRepro
 {
     public static class ReproTests
     {
         [Test]
-        public static void Test_01() => Thread.Sleep(385);
+        public static void A__Timing_test_1() => Thread.Sleep(45);
 
         [Test]
-        public static void Test_02() => Thread.Sleep(45);
+        public static void A__Timing_test_2() => Thread.Sleep(66);
 
         [Test]
-        public static void Test_03() => Thread.Sleep(66);
+        public static void A__Timing_test_3() => Thread.Sleep(80);
 
         [Test]
-        public static void Test_04() => Thread.Sleep(80);
+        public static void A__Timing_test_4() => Thread.Sleep(75);
 
         [Test]
-        public static void Test_05() => Thread.Sleep(75);
+        public static void A__Timing_test_5() => Thread.Sleep(82);
 
         [Test]
-        public static void Test_06() => Thread.Sleep(82);
+        public static void A__Timing_test_6() => Thread.Sleep(69);
 
         [Test]
-        public static void Test_07() => Thread.Sleep(69);
+        public static void A__Timing_test_7() => Thread.Sleep(75);
 
         [Test]
-        public static void Test_08() => Thread.Sleep(75);
-
-        [Test]
-        public static void Test_09()
+        public static void B__Racing_tests([Range(1, 2)] int x)
         {
             new StackTrace(true);
         }
 
         [Test]
-        public static void Test_10()
-        {
-            new StackTrace(true);
-        }
-
-        [Test]
-        public static void Tests_at_end([Range(1, 24)] int x)
+        public static void C__Ending_tests([Range(1, 24)] int x)
         {
             if (x > 12) Thread.Sleep(20);
         }
